ec64f6872c4d348813e5c617317f3ef7b5e72e6e
Sven Wappler <wappler@wappler.systems>
2024-09-25 17:30:49 +0200

    new 13 branch

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,15 +4,14 @@ $EM_CONF['font_downloader'] = [
     'title' => 'Font downloader for TYPO3',
     'description' => '',
     'category' => 'fe',
-    'version' => '12.0.1',
+    'version' => '13.0.0',
     'state' => 'stable',
     'author' => 'Sven Wappler',
     'author_email' => 'typo3YYYY@wappler.systems',
     'author_company' => 'WapplerSystems',
     'constraints' => [
         'depends' => [
-            'php' => '8.0.0-8.2.99',
-            'typo3' => '12.0.0-12.4.99',
+            'typo3' => '13.0.0-13.4.99',
         ],
     ],
 ];

6626e415875ed780cea2a91d3580446bde0223d6
Sven Wappler <wappler@wappler.systems>
2026-04-13 20:10:02 +0200

    feat: update version to 14.0.0 and refactor RenderPreProcessorHook for improved cache handling

--- Classes/Hooks/RenderPreProcessorHook.php
+++ Classes/Hooks/RenderPreProcessorHook.php
@@ -2,64 +2,47 @@
 
 namespace WapplerSystems\FontDownloader\Hooks;
 
-
-use TYPO3\CMS\Core\Cache\Backend\FileBackend;
 use TYPO3\CMS\Core\Cache\CacheManager;
 use TYPO3\CMS\Core\Cache\Exception;
 use TYPO3\CMS\Core\Cache\Exception\InvalidDataException;
-use TYPO3\CMS\Core\Cache\Exception\NoSuchCacheException;
+use TYPO3\CMS\Core\Cache\Frontend\FrontendInterface;
 use TYPO3\CMS\Core\Http\ApplicationType;
 use TYPO3\CMS\Core\Page\PageRenderer;
-use TYPO3\CMS\Core\Resource\Exception\FileDoesNotExistException;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use WapplerSystems\FontDownloader\FontLoader\FontLoaderRegistry;
 
 /**
- *
  * @author Sven Wappler <typo3YYYY@wapplersystems.de>
- *
  */
-class RenderPreProcessorHook
+readonly class RenderPreProcessorHook
 {
+    private FrontendInterface $cache;
 
-    protected FontLoaderRegistry $fontLoaderRegistry;
-
-    public function __construct()
-    {
-        $this->fontLoaderRegistry = GeneralUtility::makeInstance(FontLoaderRegistry::class);
+    public function __construct(
+        private FontLoaderRegistry $fontLoaderRegistry,
+        CacheManager $cacheManager,
+    ) {
+        $this->cache = $cacheManager->getCache('font-downloader');
     }
 
-    /**
-     * Main hook function
-     *
-     * @param array $params Array of CSS/javascript and other files
-     * @param PageRenderer $pageRenderer Pagerenderer object
-     * @return void
-     * @throws FileDoesNotExistException
-     * @throws NoSuchCacheException
-     */
     public function renderPreProcessorProc(array &$params, PageRenderer $pageRenderer): void
     {
-        if ($GLOBALS['TYPO3_REQUEST'] == null ||
+        if ($GLOBALS['TYPO3_REQUEST'] === null ||
             !ApplicationType::fromRequest($GLOBALS['TYPO3_REQUEST'])->isFrontend()
         ) {
             return;
         }
 
-        /** @var FileBackend $cache */
-        $cache = GeneralUtility::makeInstance(CacheManager::class)->getCache('font-downloader');
-
         $cssFiles = $params['cssFiles'];
         foreach ($cssFiles as $file => $cssFile) {
 
-            if (!str_starts_with($file,'https://') && !str_starts_with($file,'http://')) {
+            if (!str_starts_with($file, 'https://') && !str_starts_with($file, 'http://')) {
                 continue;
             }
 
             $cacheKey = hash('sha1', $file);
-            if ($cache->has($cacheKey)) {
-                $path = $cache->get($cacheKey);
-                $pageRenderer->addCssFile($path, $cssFile['rel'] ?? 'stylesheet', $cssFile['media'] ?? 'all', $cssFile['title'] ?? '', $cssFile['compress'] ?? true, $cssFile['forceOnTop'] ?? false, $cssFile['allWrap'] ?? '', $cssFile['excludeFromConcatenation'] ?? false, $cssFile['splitChar'] ?? '|', $cssFile['inline'] ?? false);
+            if ($this->cache->has($cacheKey)) {
+                $path = $this->cache->get($cacheKey);
+                $pageRenderer->addCssFile($path, $cssFile['rel'] ?? 'stylesheet', $cssFile['media'] ?? 'all', $cssFile['title'] ?? '', forceOnTop: $cssFile['forceOnTop'] ?? false, allWrap: $cssFile['allWrap'] ?? '', splitChar: $cssFile['splitChar'] ?? '|', inline: $cssFile['inline'] ?? false);
                 unset($params['cssFiles'][$file]);
                 continue;
             }
@@ -71,19 +54,17 @@ class RenderPreProcessorHook
                     $fontLoader->isResponsible($file)
                     && ($path = $fontLoader->load($file))
                 ) {
-                    $pageRenderer->addCssFile($path, $cssFile['rel'] ?? 'stylesheet', $cssFile['media'] ?? 'all', $cssFile['title'] ?? '', $cssFile['compress'] ?? true, $cssFile['forceOnTop'] ?? false, $cssFile['allWrap'] ?? '', $cssFile['excludeFromConcatenation'] ?? false, $cssFile['splitChar'] ?? '|', $cssFile['inline'] ?? false);
+                    $pageRenderer->addCssFile($path, $cssFile['rel'] ?? 'stylesheet', $cssFile['media'] ?? 'all', $cssFile['title'] ?? '', forceOnTop: $cssFile['forceOnTop'] ?? false, allWrap: $cssFile['allWrap'] ?? '', splitChar: $cssFile['splitChar'] ?? '|', inline: $cssFile['inline'] ?? false);
 
                     try {
-                        $cache->set($cacheKey, $path, ['fontloader'], 0);
-                    } catch (InvalidDataException $e) {
-                    } catch (Exception $e) {
+                        $this->cache->set($cacheKey, $path, ['fontloader'], 0);
+                    } catch (InvalidDataException) {
+                    } catch (Exception) {
                     }
 
                     unset($params['cssFiles'][$file]);
                 }
             }
-
         }
     }
-
 }
--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,14 @@ $EM_CONF['font_downloader'] = [
     'title' => 'Font downloader for TYPO3',
     'description' => '',
     'category' => 'fe',
-    'version' => '13.0.1',
+    'version' => '14.0.0',
     'state' => 'stable',
     'author' => 'Sven Wappler',
     'author_email' => 'typo3YYYY@wappler.systems',
     'author_company' => 'WapplerSystems',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.0.0-13.4.99',
+            'typo3' => '14.0.0-14.4.99',
         ],
     ],
 ];
